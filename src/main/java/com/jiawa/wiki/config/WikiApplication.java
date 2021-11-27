a8bad42f16c26d70c53f30ce3a70c7502d97481e
Undefeated604 <3313136283@qq.com>
2021-11-27 18:03:24 +0800

    3-5 mybatis使用示例：开发/test/list接口，查询test表的所有数据

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +14,7 @@ import org.springframework.core.env.Environment;
 @ComponentScan("com.jiawa.wiki")
 //用@ComponentScan{("com.jiawa.wiki"), "com.jiawa.controller"}可以扫描多个包
 @SpringBootApplication
+@MapperScan("com.jiawa.wiki.mapper") //扫描注解
 public class WikiApplication {
 
 
