462147e10ec1f5bd38803f5707444a6bff747d8b
Undefeated604 <3313136283@qq.com>
2021-12-23 13:50:11 +0800

    6-2 将页面改为小写字母开头，风格如：page.vue, my-page.vue

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // 下面一行代码为懒加载，文件不是很大可以不用懒加载
+    // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
