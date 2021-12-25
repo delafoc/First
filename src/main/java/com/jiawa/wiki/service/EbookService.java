be9822a8594f46ce0ffb76df765daf99135ac9a7
Undefeated604 <3313136283@qq.com>
2021-12-25 15:34:25 +0800

    6-4 集成PageHelper插件 将mapper日志级别改为trace，打印所有的sql日志

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,11 +19,15 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
 //    @Autowired // 与@Resource两个都可以
     @Resource // 注入EbookMapper
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
+        // 表示第一页数组从1开始 而且只能查下面的第一条数据库查询语句， 最好就放在查询语句的前一行
+//        PageHelper.startPage(1, 3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         // 上面的两行的写法固定
@@ -27,7 +35,15 @@ public class EbookService {
             // 参数传递模糊查询
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList); // 得到PageInfo， 需要把列表数据放进来
+
+        //打印日志，输出总行数和总页数
+        LOG.info("总行数：{}",pageInfo.getTotal()); // 写日志用占位符{}相连， 而不用+号
+        LOG.info("总页数：{}",pageInfo.getPages()); // 总页数
+
 
         //实现返回类的封装
 //        List<EbookResp> respList = new ArrayList<>();
